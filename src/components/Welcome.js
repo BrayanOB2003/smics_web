9357f552fdff20cbafabf3602f940ecf671cf5c8
BrayanOB2003 <brayanortizbastidas82@gmail.com>
2023-01-07 01:29:30 -0500

    Welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,16 @@
 import React from "react";
+import "../css_stylesheets/Welcome.css"
 
 function Welcome() {
     return(
     <div className='primary'>
-      <img 
-        className='logo-img'
-        src={require('../imgs/logo.png')}
-        alt='Logo'
-      />
-      <button className="register-button">Registrar</button>
+        <h1>Bienvenidos</h1>
+        <img 
+            className='logo-img'
+            src={require('../imgs/logo.png')}
+            alt='Logo'
+        />
+        <button className="register-button">Registrar</button>
     </div>
     );
   }
